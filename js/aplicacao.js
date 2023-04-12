18f3e98fd58987a5afdfbce691f5d1162568aee8
William Ferreira Dantas Neto <master17ce@hotmail.com>
2023-04-12 17:06:48 -0300

    correction

--- js/aplicacao.js
+++ js/aplicacao.js
@@ -1,10 +1,35 @@
-async function loadData() {
+const usuario = document.getElementById('titulo');
+const socialMedia = document.getElementById('socialMedia');
+const github = document.getElementById('github');
 
-    const {data, error} = await _supabase.from("users").select();
+async function loadData(name) {
+    const {data, error} = await _supabase.from("users").select().eq('name', name);
 
-    console.table(data);
-    console.log(error);
+    console.log(data)
+
+    data.map((dado)=>{
+        usuario.innerHTML += `<span>${dado.name}</span>`;
+        const link = document.createElement('a');
+        link.id = 'linkedin'
+        link.classList = 'bg-gray-600 rounded w-full text-white py-3 px-4 mt-5 mr-5 hover:bg-black mr-5 ml-5';
+        link.href = `${dado.linkedin_url}`;
+        link.target = "_blank"
+        link.innerHTML = 'LinkedIn';
+        document.getElementById('socialMedia').appendChild(link);
+        const git = document.createElement('a');
+        git.id = 'linkedin'
+        git.classList = 'bg-gray-600 rounded w-full text-white py-3 px-4 mt-5 mr-5 hover:bg-black mr-5 ml-5';
+        git.href = `${dado.github_url}`;
+        git.target = "_blank"
+        git.innerHTML = 'GitHub';
+        document.getElementById('socialMedia').appendChild(git);
+    })
 
 }
 
-loadData()
+const urlParams = new URLSearchParams(window.location.search);
+
+const nomeParam = urlParams.get("name")
+
+loadData(nomeParam)
+
--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 const form = document.getElementById('generate-form');
 const qr = document.getElementById('qrcode');
-
 const onGenerateSubmit = (e) => {
     e.preventDefault();
-
     clearUI();
-
-    const url = document.getElementById('url').value;
+    const name = document.getElementById('name').value;
     const size = document.getElementById('size').value;
+    const linkedin = document.getElementById('linkedin').value;
+    const github = document.getElementById('github').value;
+    const url = "http://127.0.0.1:5500/Perfil.html?name=" + name;
 
-    if (url === '') {
+    const data = {
+        name: name,
+        linkedin_url: linkedin,
+        github_url: github
+    }
+    if (name === '') {
         alert('Please enter a user name');
     } else {
+        insertData(data);
         showSpinner();
-
         setTimeout(() => {
             hideSpinner();
-
             generateQRCode(url, size);
 
             setTimeout(() => {
@@ -34,22 +38,17 @@ const generateQRCode = (url, size) => {
         height: size
     });
 };
-
 const showSpinner = () => {
     document.getElementById('spinner').style.display = 'block';
 };
-
 const hideSpinner = () => {
     document.getElementById('spinner').style.display = 'none';
 };
-
 const clearUI = () => {
     qr.innerHTML = '';
     const saveLink = document.getElementById('save-link');
     if (saveLink) saveLink.remove();
-
 };
-
 const createSaveBtn = (saveUrl) => {
     const link = document.createElement('a');
     link.id = 'save-link'
@@ -59,8 +58,18 @@ const createSaveBtn = (saveUrl) => {
     link.innerHTML = 'Save Image';
     document.getElementById('generated').appendChild(link);
 };
-
 hideSpinner();
 
 form.addEventListener('submit', onGenerateSubmit);
 
+async function insertData(data) {
+
+    const {error} = await _supabase.from("users").insert([data]);
+
+    if (error) {
+        console.log(error);
+        return;
+    } else {
+        console.log("Dados salvos com sucessor!");
+    }
+}
